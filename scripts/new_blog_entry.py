cd30ba925af8551b35c937e34dcfc28ebb218468
Darren Hoyland <darren.hoyland@canonical.com>
2016-01-25 11:17:30 +0000

    Added genetics thing from 2013

--- scripts/new_blog_entry.py
+++ scripts/new_blog_entry.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from new_entry import create_entry
 
 folder = 'markdown'
-filename = create_entry(folder, category='Blog')
+filename = 'blog_' + datetime.now().strftime("%Y-%m-%d")
+path_to_file = create_entry(folder, filename=filename, category='Blog')
 
-Popen(["gedit", filename])
+Popen(["gedit", path_to_file])
