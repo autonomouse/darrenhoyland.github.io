5259e822206460930c834c2358053a84288ab548
Darren Hoyland <darren.hoyland@canonical.com>
2016-02-07 22:36:21 +0000

    removed my filepath as default

--- scripts/new_personal_entry.py
+++ scripts/new_personal_entry.py
@@ -4,7 +4,7 @@ from subprocess import Popen
 from datetime import datetime
 from new_entry import create_entry
 
-folder = '/home/darren/Repositories/Journals/Journal/markdown'
+folder = 'markdown'
 filename = 'personal_' + datetime.now().strftime("%Y-%m-%d")
 path_to_file = create_entry(folder, filename=filename, category='Personal')
 
