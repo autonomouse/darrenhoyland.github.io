2e9eb8fa198e02edf2f98d458b1de87323605098
Darren Hoyland <darren.hoyland@canonical.com>
2016-02-07 22:28:37 +0000

    Made new_entry script more generic

--- scripts/new_entry.py
+++ scripts/new_entry.py
@@ -4,7 +4,9 @@ import os
 from datetime import datetime
 
 
-def create_entry(folder, timestamp=None, title=None, filename=None, category=''):
+def create_entry(folder, timestamp='', title='', filename=None, category='',
+                 name='', tags='', header_image='', categories='', 
+                 publish_bool='True', content=''):
 
     try:
         os.makedirs(folder)
@@ -23,17 +25,20 @@ def create_entry(folder, timestamp=None, title=None, filename=None, category='')
     if os.path.exists(filepath):
         return filepath
     
-    meta =  "Title:          {}\n"
-    meta += "Authors:        Darren Hoyland\n"
-    meta += "Date:           {}\n"
-    meta += "Tags:           \n"
-    meta += "HeaderImage:    \n"
-    meta += "Category:       {}\n"
-    meta += "Publish:        True\n"
+    meta =  "Title:          {title}\n"
+    meta += "Authors:        {name}\n"
+    meta += "Date:           {date}\n"
+    meta += "Tags:           {tags}\n"
+    meta += "HeaderImage:    {header_image}\n"
+    meta += "Category:       {categories}\n"
+    meta += "Publish:        {publish_bool}\n"
     meta += "\n\n"
-    meta = meta.format(title, timestamp, category)        
+    meta = meta.format(title=title, name=name, date=timestamp, tags=tags, 
+                       header_image=header_image, categories=categories, 
+                       publish_bool=publish_bool)        
     
     with open(filepath, 'w') as op:
-        op.write(meta)       
+        op.write(meta)
+        op.write(content)       
     
     return filepath
